4bceb0e9573acd4ab97d2baa0ca2c37469ee2f95
justabitcoiner <justabitcoiner@gmail.com>
2024-07-06 19:47:06 +0700

    singleton

--- creational/singleton.py
+++ creational/singleton.py
@@ -1,23 +1,25 @@
 # Singleton is a creational design pattern that lets you ensure that a class has only one instance,
 # while providing a global access point to this instance.
-from random import random
+
+
+class DatabaseConnection:
+    pass
 
 
 class Database:
-    con = None
+    __connection: DatabaseConnection = None
 
     @classmethod
     def get_connection(cls):
-        print(cls.con)
-        if not cls.con:
-            cls.con = random()
-        return cls.con
+        if not cls.__connection:
+            cls.__connection = DatabaseConnection()
+        return cls.__connection
 
 
 def main():
-    Database.get_connection()
-    Database.get_connection()
-    Database.get_connection()
+    print(Database.get_connection())
+    print(Database.get_connection())
+    print(Database.get_connection())
 
 
 main()
